091e402222ec5f0426fefd42f83caf0c38f7d7ae
Richard Arthur <richardarthur228@gmail.com>
2022-07-12 09:53:36 +0000

    added firebase.js

---
+++ discord-ui-clone/lib/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA1ItMYRLm1zKdpHSfz4G5iV6Pt2aMH-E",
+  authDomain: "clone-fc004.firebaseapp.com",
+  projectId: "clone-fc004",
+  storageBucket: "clone-fc004.appspot.com",
+  messagingSenderId: "525288206168",
+  appId: "1:525288206168:web:bb4f30429f17565c8dff2d",
+  measurementId: "G-BKNNDE8079",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
