be392c90c13146fdab23b61d12d678aa8ffe2df2
Richard Arthur <richardarthur228@gmail.com>
2024-01-09 14:17:57 -0600

    add discord clone

--- disney-clone/lib/getImagePath.ts
+++
@@ -1,5 +0,0 @@
-export const getImagePath = (path?: string, fullSize?: boolean) => {
-  return path
-    ? `http://image.tmdb.org/t/p/${fullSize ? "original" : "w500"}/${path}`
-    : "https://links.papareact.com/o8z";
-};
