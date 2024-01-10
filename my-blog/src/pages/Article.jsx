5be3215b7548e34afad96d872ee664fd1324f4f1
Richard Arthur <richardarthur228@gmail.com>
2024-01-10 05:48:35 -0600

    Add Article page

---
+++ my-blog/src/pages/Article.jsx
@@ -0,0 +1,5 @@
+function Article() {
+  return <div>Article Page</div>;
+}
+
+export default Article;
--- my-blog/src/pages/ArticleList.jsx
+++ my-blog/src/pages/ArticleList.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function ArticleList() {
   return <div>ArticleList</div>;
 }
